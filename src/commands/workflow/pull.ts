a4829662d8c603f512d8d46e81e1d75f13a73627
Thomas <thomas.whyyou@gmail.com>
2022-12-14 21:00:01 -0500

    wip: write workflow file

---
+++ src/commands/workflow/pull.ts
@@ -0,0 +1,30 @@
+import { Flags } from '@oclif/core'
+import * as fs from "fs-extra";
+
+import BaseCommand from "@/lib/base-command";
+
+export default class WorkflowPull extends BaseCommand {
+  static flags = {
+    "hide-uncommitted-changes": Flags.boolean(),
+  }
+
+  static args = [
+    { name: 'workflowKey', required: true }
+  ]
+
+  async run(): Promise<void> {
+    const flags = { ...this.props.flags, annotate: true };
+
+    const resp = await this.apiV1.getWorkflow({ ...this.props, flags });
+
+    const { workflowKey } = this.props.args;
+    const workflowFilePath = `./${workflowKey}/workflow.json`
+
+    // XXX: Strip out annotations and extract out templates..
+    try {
+      await fs.outputJson(workflowFilePath, resp.data, {spaces: "\t"})
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
