53502fbe66ecf838d963747b5550ad8928985a2e
Thomas <thomas.whyyou@gmail.com>
2022-12-21 00:55:17 -0500

    turn flag helpers to generic pagination helpers

--- src/lib/helpers/pagination-helpers.ts
+++ src/lib/helpers/pagination-helpers.ts
@@ -1,6 +1,15 @@
 import { Flags, Interfaces } from "@oclif/core";
 import { pick } from "lodash";
 
+export type Paginated<T = any> = {
+  entries: T[];
+  page_info: {
+    after: string | null;
+    before: string | null;
+    page_size: number;
+  };
+};
+
 export const paginationFlags = {
   after: Flags.string(),
   before: Flags.string(),
