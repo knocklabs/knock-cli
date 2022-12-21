b03412a04fbdac9cc4600b9b9b5067cf49725575
Thomas <thomas.whyyou@gmail.com>
2022-12-21 01:55:32 -0500

    add formatDateTime helper with a test

--- src/lib/helpers/date-helpers.ts
+++ src/lib/helpers/date-helpers.ts
@@ -3,3 +3,7 @@ import { format, parseISO } from "date-fns";
 export function formatDate(input: string): string {
   return format(parseISO(input), "MMM d, yyyy");
 }
+
+export function formatDateTime(input: string): string {
+  return format(parseISO(input), "MMM d, yyyy HH:mm:ss");
+}
--- test/lib/helpers/date-helpers.test.ts
+++ test/lib/helpers/date-helpers.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "@oclif/test";
 
-import { formatDate } from "@/lib/helpers/date-helpers";
+import { formatDate, formatDateTime } from "@/lib/helpers/date-helpers";
 
 describe("date-helpers", () => {
   describe("formatDate", () => {
@@ -9,6 +9,16 @@ describe("date-helpers", () => {
         const result = formatDate("2022-11-14T21:02:23.117949Z")
 
         expect(result).to.equal("Nov 14, 2022");
+      });
+    });
+  });
+
+  describe("formatDateTime", () => {
+    describe("given a iso 8601 datetime string", () => {
+      it("returns a human readable datetime string in MMM d, yyyy HH:mm:ss format", () => {
+        const result = formatDateTime("2022-11-14T21:02:23.117949Z")
+
+        expect(result).to.equal("Nov 14, 2022 16:02:23");
       });
     });
   });
