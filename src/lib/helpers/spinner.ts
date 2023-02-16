79a91ec07406d8677bfc86944a5ae4e2995d9e37
Thomas <thomas.whyyou@gmail.com>
2023-02-16 01:06:05 -0500

    more cleanups

--- src/lib/helpers/spinner.ts
+++ src/lib/helpers/spinner.ts
@@ -2,7 +2,8 @@ import { CliUx } from "@oclif/core";
 
 import { isTestEnv } from "./env";
 
-export const start = (action: string): void =>
-  !isTestEnv && CliUx.ux.action.start(action);
+export const start = (action: string): void => {
+  if (!isTestEnv) CliUx.ux.action.start(action);
+};
 
 export const stop = (): void => CliUx.ux.action.stop();
