b0ded0d4cf8ba16c3d6253148742b670708a551d
Thomas <thomas.whyyou@gmail.com>
2023-05-02 17:22:45 -0400

    add tests for isDirectory helper

---
+++ test/lib/helpers/fs.test.ts
@@ -0,0 +1,48 @@
+import { expect } from "@oclif/test";
+
+import * as path from "node:path";
+import * as fs from "fs-extra";
+import { sandboxDir } from "@/lib/helpers/const";
+
+import { isDirectory } from "@/lib/helpers/fs";
+
+const currCwd = process.cwd();
+
+describe("lib/helpers/fs", () => {
+  describe("isDirectory", () => {
+    beforeEach(() => {
+      fs.removeSync(sandboxDir);
+      fs.ensureDirSync(sandboxDir);
+    });
+    afterEach(() => {
+      process.chdir(currCwd);
+      fs.removeSync(sandboxDir);
+    });
+
+    describe("given a nonexistent path", () => {
+      it("returns false", async () => {
+        const abspath = path.resolve(sandboxDir, "foo");
+
+        expect(await isDirectory(abspath)).to.equal(false)
+      });
+    });
+
+    describe("given a path to a file", () => {
+      it("returns false", async () => {
+        const abspath = path.resolve(sandboxDir, "foo");
+        fs.ensureFileSync(abspath)
+
+        expect(await isDirectory(abspath)).to.equal(false)
+      });
+    });
+
+    describe("given a path to a directory", () => {
+      it("returns false", async () => {
+        const abspath = path.resolve(sandboxDir, "foo");
+        fs.ensureDirSync(abspath)
+
+        expect(await isDirectory(abspath)).to.equal(true)
+      });
+    });
+  });
+});
