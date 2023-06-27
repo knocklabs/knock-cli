5575fbb90a648ca2ba9ebc4a9423789251f06e19
Billy Ceskavich <bceskavich@gmail.com>
2023-06-27 15:54:13 -0400

    update 'workflow get' command for if-else steps

--- src/commands/workflow/get.ts
+++ src/commands/workflow/get.ts
@@ -7,6 +7,18 @@ import { withSpinner } from "@/lib/helpers/request";
 import * as Conditions from "@/lib/marshal/conditions";
 import * as Workflow from "@/lib/marshal/workflow";
 
+type WorkflowStepWithKey = Workflow.WorkflowStepData & { key: string };
+
+type WorkflowBranchWithKey = Workflow.WorkflowBranch & {
+  type: "branch";
+  isDefault: boolean;
+  key: string;
+};
+
+type WorkflowStepOrBranchWithKey = WorkflowStepWithKey | WorkflowBranchWithKey;
+
+const LOWERCASE_A_CHAR_CODE = 97;
+
 export default class WorkflowGet extends BaseCommand<typeof WorkflowGet> {
   static summary = "Display a single workflow from an environment.";
 
@@ -108,30 +120,96 @@ export default class WorkflowGet extends BaseCommand<typeof WorkflowGet> {
      * Workflow steps table
      */
 
-    const steps = workflow.steps.map((step, index) => ({ ...step, index }));
+    const { steps } = this.flattenSteps(workflow.steps);
 
     ux.table(steps, {
       index: {
         header: "Steps",
-        get: (step) => step.index + 1,
+        get: (stepOrBranch) => stepOrBranch.key,
       },
       ref: {
         header: "Ref",
         minWidth: 18,
+        get: (stepOrBranch) =>
+          stepOrBranch.type === "branch" ? "" : stepOrBranch.ref,
       },
       type: {
         header: "Type",
         minWidth: 12,
+        get: (stepOrBranch) =>
+          stepOrBranch.type === "branch" ? "" : stepOrBranch.type,
       },
       summary: {
         header: "Summary",
-        get: (step) => Workflow.formatStepSummary(step),
+        get: (stepOrBranch) =>
+          stepOrBranch.type === "branch"
+            ? Workflow.formatBranchSummary(stepOrBranch)
+            : Workflow.formatStepSummary(stepOrBranch),
       },
       conditions: {
         header: "Conditions",
-        get: (step) =>
-          step.conditions ? Conditions.formatConditions(step.conditions) : "-",
+        get: (stepOrBranch) => {
+          if (stepOrBranch.type === Workflow.StepType.IfElse) return "-";
+
+          if (stepOrBranch.type === "branch" && stepOrBranch.isDefault) {
+            return "Default branch";
+          }
+
+          if (!stepOrBranch.conditions) return "-";
+
+          return Conditions.formatConditions(stepOrBranch.conditions);
+        },
       },
     });
+  }
+
+  // Flattens a nested list of steps and branches for a workflow tree into a
+  // simple list + also includes some formatting logic for the step/branch
+  // "level" values
+  private flattenSteps(
+    steps: Workflow.WorkflowStepData[],
+    totalSteps = 0,
+    parentBranches: string[] = [],
+  ): { steps: WorkflowStepOrBranchWithKey[]; totalSteps: number } {
+    const colsIndented = parentBranches.length * 2;
+    // eslint-disable-next-line unicorn/no-new-array
+    const indentationPrefix = new Array(colsIndented).fill("-").join("");
+
+    let result: WorkflowStepOrBranchWithKey[] = [];
+
+    for (const step of steps) {
+      totalSteps += 1;
+
+      const stepKey = `${indentationPrefix}${totalSteps}`;
+      result.push({ ...step, key: stepKey });
+
+      if (step.type === Workflow.StepType.IfElse) {
+        const totalStepsForBranch = totalSteps;
+
+        for (let branchIdx = 0; branchIdx < step.branches.length; branchIdx++) {
+          const branchLetter = String.fromCharCode(
+            LOWERCASE_A_CHAR_CODE + branchIdx,
+          );
+          const branchName = `${totalStepsForBranch}${branchLetter}`;
+          const allBranches = [...parentBranches, branchName];
+
+          const branch = step.branches[branchIdx];
+          const { steps: nestedSteps, totalSteps: nextTotalSteps } =
+            this.flattenSteps(branch.steps, totalSteps, allBranches);
+
+          const branchWithKey: WorkflowBranchWithKey = {
+            ...branch,
+            type: "branch",
+            isDefault: branchIdx === step.branches.length - 1,
+            key: `${indentationPrefix}--branch_${allBranches.join("_")}\n`,
+          };
+
+          result = [...result, branchWithKey, ...nestedSteps];
+          totalSteps = nextTotalSteps;
+        }
+      }
+    }
+
+    return { steps: result, totalSteps: totalSteps };
   }
 }
