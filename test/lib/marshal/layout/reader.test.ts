be9082471e5e5f5bcf1874651cfa276276f1d7ac
Franco Borrelli <franco@mimiquate.com>
2023-10-03 14:17:28 -0300

    feat(KNO-4367): add email layout reader test

---
+++ test/lib/marshal/layout/reader.test.ts
@@ -0,0 +1,179 @@
+import * as path from "node:path";
+
+import { expect } from "@oclif/test";
+import * as fs from "fs-extra";
+import { get } from "lodash";
+
+import { xpath } from "@/../test/support";
+import { sandboxDir } from "@/lib/helpers/const";
+import { JsonDataError } from "@/lib/helpers/error";
+import { LAYOUT_JSON } from "@/lib/marshal/email-layout";
+import { readExtractedFileSync } from "@/lib/marshal/shared/helpers";
+import { EmailLayoutDirContext } from "@/lib/run-context";
+import { readEmailLayoutDir } from "@/lib/marshal/email-layout/reader";
+
+const currCwd = process.cwd();
+
+describe("lib/marshal/layout/reader", () => {
+  describe("readExtractedFileSync", () => {
+    const emailLayoutDirCtx: EmailLayoutDirContext = {
+      type: "email_layout",
+      key: "transactional",
+      abspath: path.resolve(sandboxDir, "transactional"),
+      exists: true,
+    };
+
+    beforeEach(() => {
+      fs.removeSync(sandboxDir);
+      fs.ensureDir(emailLayoutDirCtx.abspath);
+    });
+    after(() => fs.removeSync(sandboxDir));
+
+    describe("given a valid liquid text layout", () => {
+      it("returns the read text layout content without errors", async () => {
+        const fileContent = `
+          {{ vars.app_name }} ({{ vars.app_url }})
+          <p>
+          Hello {{ recipient.name }}
+          </p>
+       `.trimStart();
+
+        const filePath = path.resolve(emailLayoutDirCtx.abspath, "sample.txt");
+        fs.outputFileSync(filePath, fileContent);
+
+        const [readContent, error] = readExtractedFileSync(
+          "sample.txt",
+          emailLayoutDirCtx,
+        );
+
+        expect(readContent).to.be.a("string");
+        expect(error).to.equal(undefined);
+      });
+    });
+
+    describe("given an invalid liquid text layout", () => {
+      it("returns the read text layout content with errors", async () => {
+        const fileContent = `
+          {{ vars.app_name }} ({{ vars.app_url }})
+          <p>
+          Hello {{ recipient.name 
+          </p>
+       `.trimStart();
+
+        const filePath = path.resolve(emailLayoutDirCtx.abspath, "sample.txt");
+        fs.outputFileSync(filePath, fileContent);
+
+        const [readContent, error] = readExtractedFileSync(
+          "sample.txt",
+          emailLayoutDirCtx,
+        );
+
+        expect(readContent).to.equal(undefined);
+        expect(error).to.be.an.instanceof(JsonDataError);
+      });
+    });
+  });
+
+  describe("readEmailLayoutDir", () => {
+    const sampleEmailLayoutJson = {
+      name: "Transactional",
+      "html_layout@": "html_layout.html",
+      "text_layout@": "text-layout-examples/text_layout.txt",
+      __readonly: {
+        key: "transactional",
+        environment: "development",
+        created_at: "2023-09-18T18:32:18.398053Z",
+        updated_at: "2023-10-02T19:24:48.714630Z",
+      },
+    };
+
+    const emailLayoutDirPath = path.join(
+      sandboxDir,
+      "layouts",
+      "transactional",
+    );
+
+    const emailLayoutDirCtx: EmailLayoutDirContext = {
+      type: "email_layout",
+      key: "transactional",
+      abspath: emailLayoutDirPath,
+      exists: true,
+    };
+
+    before(() => {
+      fs.removeSync(sandboxDir);
+
+      // Set up a sample layout directory
+      fs.outputJsonSync(
+        path.join(emailLayoutDirPath, LAYOUT_JSON),
+        sampleEmailLayoutJson,
+      );
+
+      fs.outputJsonSync(
+        path.join(emailLayoutDirPath, "html_layout.html"),
+        "<html><body><p> example </p></body></html>",
+      );
+
+      fs.outputJsonSync(
+        path.join(
+          emailLayoutDirPath,
+          "text-layout-examples",
+          "text_layout.txt",
+        ),
+        "foo {{content}}",
+      );
+    });
+
+    after(() => {
+      process.chdir(currCwd);
+      fs.removeSync(sandboxDir);
+    });
+
+
+    describe("by default without any opts", () => {
+      it("reads layout.json without the readonly field and extracted files joined", async () => {
+        const [layout] = await readEmailLayoutDir(emailLayoutDirCtx);
+
+        expect(get(layout, ["name"])).to.equal("Transactional");
+        expect(get(layout, ["html_layout@"])).to.equal("html_layout.html");
+        expect(get(layout, ["text_layout@"])).to.equal("text-layout-examples/text_layout.txt");
+      });
+    });
+
+    describe("with the withReadonlyField opt of true", () => {
+      it("reads layout.json with the readonly field", async () => {
+        const [layout] = await readEmailLayoutDir(emailLayoutDirCtx, {
+          withReadonlyField: true,
+        });
+
+        expect(get(layout, ["name"])).to.equal("Transactional");
+        expect(get(layout, ["html_layout@"])).to.equal("html_layout.html");
+        expect(get(layout, ["text_layout@"])).to.equal("text-layout-examples/text_layout.txt");
+
+        expect(get(layout, ["__readonly"])).to.eql({
+          key: "transactional",
+          environment: "development",
+          created_at: "2023-09-18T18:32:18.398053Z",
+          updated_at: "2023-10-02T19:24:48.714630Z"
+        });
+      });
+
+      describe("with the withExtractedFiles opt of true", () => {
+        it("reads layout.json with the extracted fields inlined", async () => {
+          const [layout] = await readEmailLayoutDir(emailLayoutDirCtx, {
+            withExtractedFiles: true,
+          });
+
+          expect(get(layout, ["name"])).to.equal("Transactional");
+
+          // HTML layout content should be inlined into layout data
+          expect(get(layout, ["html_layout"])).to.contain("<html><body><p> example </p></body></html>");
+
+          // Text layout content should be inlined into layout data
+          expect(get(layout, ["text_layout"])).to.contains("foo {{content}}");
+        });
+      });
+
+    });
+  });
+});
