a2a66765dbb351ccb2e00504568bc1b6f935688a
Thomas <thomas.whyyou@gmail.com>
2023-12-26 16:38:00 -0800

    add and export buildTranslationDirBundle helper

---
+++ src/lib/marshal/translation/processor.isomorphic.ts
@@ -0,0 +1,56 @@
+import { TranslationData } from "./types";
+
+type TranslationDirBundle = {
+  [relpath: string]: string;
+};
+
+/*
+ * Returns a formatted translation "ref".
+ */
+export const formatRef = (
+  localeCode: string,
+  namespace: string | undefined,
+): string => (namespace ? `${namespace}.${localeCode}` : localeCode);
+
+/*
+ * Returns a formatted translation file name based on the given translation ref
+ * or payload.
+ */
+export const formatFileName = (input: string | TranslationData): string => {
+  const ref =
+    typeof input === "string"
+      ? input
+      : formatRef(input.locale_code, input.namespace);
+
+  return `${ref}.json`;
+};
+
+/*
+ * Takes one or more translation payloads, and builds a "translation locale
+ * directory bundle". This is an object which contains all the relative paths of
+ * the translation file(s) and its file content.
+ *
+ * Note, all given translation(s) in a single directory bundle should be of the
+ * same locale, and this is assumed so here without further validations in this
+ */
+type OneOrMoreTranslationData = TranslationData | TranslationData[];
+
+export const buildTranslationDirBundle = (
+  input: OneOrMoreTranslationData,
+): TranslationDirBundle => {
+  if (Array.isArray(input)) {
+    const translations = input;
+
+    return Object.fromEntries(
+      translations.map((translation) => [
+        formatFileName(translation),
+        translation.content,
+      ]),
+    );
+  }
+
+  const translation = input;
+  return {
+    [formatFileName(translation)]: translation.content,
+  };
+};
