aaf456e51b128d610a338c6c7b6cde781dfbd7ef
Thomas <thomas.whyyou@gmail.com>
2024-04-30 17:47:39 -0400

    chore: exclude system translation file when reading from disk (#331)

--- src/lib/marshal/translation/reader.ts
+++ src/lib/marshal/translation/reader.ts
@@ -10,6 +10,7 @@ import {
   isTranslationDir,
   lsTranslationDir,
   parseTranslationRef,
+  SYSTEM_NAMESPACE,
   TranslationCommandTarget,
   TranslationFileContext,
 } from "./helpers";
@@ -40,6 +41,12 @@ const readTranslationFiles = async (
     if (!parsedRef) continue;
 
     const { localeCode, namespace } = parsedRef;
+
+    // Skip the system translation file when reading from the disk by default,
+    // as it is not user editable and should be excluded from the validate or
+    // push commands. Consider making this an option in the future.
+    if (namespace === SYSTEM_NAMESPACE) continue;
+
     // eslint-disable-next-line no-await-in-loop
     const [content, readJsonErrors] = await readJson(abspath);
 
---
+++ test/lib/marshal/translation/reader.test.ts
@@ -0,0 +1,63 @@
+import * as path from "node:path";
+
+import { expect } from "@oclif/test";
+import * as fs from "fs-extra";
+
+import { sandboxDir } from "@/lib/helpers/const";
+import { SYSTEM_NAMESPACE } from "@/lib/marshal/translation/helpers";
+import { readAllForCommandTarget } from "@/lib/marshal/translation/reader";
+import { TranslationDirContext } from "@/lib/run-context";
+
+const currCwd = process.cwd();
+
+describe("lib/marshal/translation/reader", () => {
+  describe("readAllForCommandTarget", () => {
+    const translationLocaleDirPath = path.join(
+      sandboxDir,
+      "translations",
+      "en",
+    );
+
+    before(() => {
+      fs.removeSync(sandboxDir);
+
+      // Set up a sample translation locale directory.
+      fs.outputJsonSync(path.join(translationLocaleDirPath, "en.json"), {
+        foo: "bar",
+      });
+      fs.outputJsonSync(path.join(translationLocaleDirPath, "hello.en.json"), {
+        foo: "bar",
+      });
+      fs.outputJsonSync(path.join(translationLocaleDirPath, "system.en.json"), {
+        foo: "bar",
+      });
+    });
+
+    after(() => {
+      process.chdir(currCwd);
+      fs.removeSync(sandboxDir);
+    });
+
+    describe("given a translation locale dir target with a system translation file", () => {
+      it("returns translation files without the system translation", async () => {
+        const translationDirCtx: TranslationDirContext = {
+          type: "translation",
+          key: "en",
+          abspath: translationLocaleDirPath,
+          exists: true,
+        };
+
+        const [translations, errors] = await readAllForCommandTarget({
+          type: "translationDir",
+          context: translationDirCtx,
+        });
+
+        expect(errors.length).to.equal(0);
+
+        expect(
+          translations.find((t) => t.namespace === SYSTEM_NAMESPACE),
+        ).to.equal(undefined);
+      });
+    });
+  });
+});
