3df69e9c34514202dc16af0ef24b3bb954d29769
Thomas <thomas.whyyou@gmail.com>
2024-12-09 18:27:10 -0500

    add message-type list and get commands

---
+++ src/commands/message-type/get.ts
@@ -0,0 +1,124 @@
+import { Args, Flags, ux } from "@oclif/core";
+
+import * as ApiV1 from "@/lib/api-v1";
+import BaseCommand from "@/lib/base-command";
+import { formatDateTime } from "@/lib/helpers/date";
+import { ApiError } from "@/lib/helpers/error";
+import { formatErrorRespMessage, isSuccessResp } from "@/lib/helpers/request";
+import { spinner } from "@/lib/helpers/ux";
+
+export default class MessageTypeGet extends BaseCommand<typeof MessageTypeGet> {
+  static summary = "Display a single in-app message type from an environment.";
+
+  static flags = {
+    environment: Flags.string({
+      default: "development",
+      summary: "The environment to use.",
+    }),
+    "hide-uncommitted-changes": Flags.boolean({
+      summary: "Hide any uncommitted changes.",
+    }),
+  };
+
+  static args = {
+    messageTypeKey: Args.string({
+      required: true,
+    }),
+  };
+
+  static enableJsonFlag = true;
+
+  async run(): Promise<ApiV1.GetMessageTypeResp | void> {
+    spinner.start("‣ Loading");
+
+    const { messageType } = await this.loadMessageType();
+
+    spinner.stop();
+
+    const { flags } = this.props;
+    if (flags.json) return messageType;
+
+    this.render(messageType);
+  }
+
+  private async loadMessageType(): Promise<{
+    messageType: ApiV1.GetMessageTypeResp;
+  }> {
+    const messageTypeResp = await this.apiV1.getMessageType(this.props);
+
+    if (!isSuccessResp(messageTypeResp)) {
+      const message = formatErrorRespMessage(messageTypeResp);
+      ux.error(new ApiError(message));
+    }
+
+    return {
+      messageType: messageTypeResp.data,
+    };
+  }
+
+  render(messageType: ApiV1.GetMessageTypeResp): void {
+    const { messageTypeKey } = this.props.args;
+    const { environment: env, "hide-uncommitted-changes": commitedOnly } =
+      this.props.flags;
+
+    const qualifier =
+      env === "development" && !commitedOnly ? "(including uncommitted)" : "";
+
+    this.log(
+      `‣ Showing in-app message type \`${messageTypeKey}\` in \`${env}\` environment ${qualifier}\n`,
+    );
+
+    /*
+     * Message type table
+     */
+
+    const rows = [
+      {
+        key: "Name",
+        value: messageType.name,
+      },
+      {
+        key: "Key",
+        value: messageType.key,
+      },
+      {
+        key: "Description",
+        value: messageType.description || "-",
+      },
+      {
+        key: "Owner",
+        value: messageType.owner,
+      },
+      {
+        key: "Created at",
+        value: formatDateTime(messageType.created_at),
+      },
+      {
+        key: "Updated at",
+        value: formatDateTime(messageType.updated_at),
+      },
+      {
+        key: "Schema",
+        value:
+          (messageType.variants || []).length > 0
+            ? "\n" + JSON.stringify(messageType.variants, null, 2)
+            : "-",
+      },
+      {
+        key: "Preview template",
+        value: messageType.preview ? "\n" + messageType.preview : "-",
+      },
+    ];
+
+    ux.table(rows, {
+      key: {
+        header: "Message type",
+        minWidth: 24,
+      },
+      value: {
+        header: "",
+        minWidth: 24,
+      },
+    });
+  }
+}
