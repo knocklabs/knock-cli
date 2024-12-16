341e95baba89333541236acc471da3d201ce058d
Thomas <thomas.whyyou@gmail.com>
2024-12-16 17:24:04 -0500

    add message-type pull command

---
+++ src/lib/marshal/message-type/helpers.ts
@@ -0,0 +1,127 @@
+import * as path from "node:path";
+
+import { ux } from "@oclif/core";
+import * as fs from "fs-extra";
+
+import { DirContext } from "@/lib/helpers/fs";
+import { MessageTypeDirContext, RunContext } from "@/lib/run-context";
+
+import { MESSAGE_TYPE_JSON } from "./processor.isomorphic";
+
+export const messageTypeJsonPath = (
+  messageTypeDirCtx: MessageTypeDirContext,
+): string => path.resolve(messageTypeDirCtx.abspath, MESSAGE_TYPE_JSON);
+
+/*
+ * Check for message_type.json file and return the file path if present.
+ */
+export const lsMessageTypeJson = async (
+  dirPath: string,
+): Promise<string | undefined> => {
+  const messageTypeJsonPath = path.resolve(dirPath, MESSAGE_TYPE_JSON);
+
+  const exists = await fs.pathExists(messageTypeJsonPath);
+  return exists ? messageTypeJsonPath : undefined;
+};
+
+/*
+ * Evaluates whether the given directory path is a message type directory, by
+ * checking for the presence of message_type.json file.
+ */
+export const isMessageTypeDir = async (dirPath: string): Promise<boolean> =>
+  Boolean(await lsMessageTypeJson(dirPath));
+
+/*
+ * Validate the provided args and flags with the current run context, to first
+ * ensure the invoked command makes sense, and return the target context.
+ */
+type CommandTargetProps = {
+  flags: {
+    all: boolean | undefined;
+    "message-types-dir": DirContext | undefined;
+  };
+  args: {
+    messageTypeKey: string | undefined;
+  };
+};
+type MessageTypeDirTarget = {
+  type: "messageTypeDir";
+  context: MessageTypeDirContext;
+};
+type MessageTypesIndexDirTarget = {
+  type: "messageTypesIndexDir";
+  context: DirContext;
+};
+export type MessageTypeCommandTarget =
+  | MessageTypeDirTarget
+  | MessageTypesIndexDirTarget;
+
+export const ensureValidCommandTarget = async (
+  props: CommandTargetProps,
+  runContext: RunContext,
+): Promise<MessageTypeCommandTarget> => {
+  const { args, flags } = props;
+  const { commandId, resourceDir: resourceDirCtx, cwd: runCwd } = runContext;
+
+  // If the target resource is a different type than the current resource dir
+  // type, error out.
+  if (resourceDirCtx && resourceDirCtx.type !== "message_type") {
+    return ux.error(
+      `Cannot run ${commandId} inside a ${resourceDirCtx.type} directory`,
+    );
+  }
+
+  // Cannot accept both message type key arg and --all flag.
+  if (flags.all && args.messageTypeKey) {
+    return ux.error(
+      `messageTypeKey arg \`${args.messageTypeKey}\` cannot also be provided when using --all`,
+    );
+  }
+
+  // --all flag is given, which means no message type key arg.
+  if (flags.all) {
+    // If --all flag used inside a message type directory, then require a message
+    // types dir path.
+    if (resourceDirCtx && !flags["message-types-dir"]) {
+      return ux.error("Missing required flag message-types-dir");
+    }
+
+    // Targeting all message type dirs in the message types index dir.
+    // TODO: Default to the knock project config first if present before cwd.
+    const defaultToCwd = { abspath: runCwd, exists: true };
+    const indexDirCtx = flags["message-types-dir"] || defaultToCwd;
+
+    return { type: "messageTypesIndexDir", context: indexDirCtx };
+  }
+
+  // Message type key arg is given, which means no --all flag.
+  if (args.messageTypeKey) {
+    if (resourceDirCtx && resourceDirCtx.key !== args.messageTypeKey) {
+      return ux.error(
+        `Cannot run ${commandId} \`${args.messageTypeKey}\` inside another message type directory:\n${resourceDirCtx.key}`,
+      );
+    }
+
+    const targetDirPath = resourceDirCtx
+      ? resourceDirCtx.abspath
+      : path.resolve(runCwd, args.messageTypeKey);
+
+    const messageTypeDirCtx: MessageTypeDirContext = {
+      type: "message_type",
+      key: args.messageTypeKey,
+      abspath: targetDirPath,
+      exists: await isMessageTypeDir(targetDirPath),
+    };
+
+    return { type: "messageTypeDir", context: messageTypeDirCtx };
+  }
+
+  // From this point on, we have neither a message type key arg nor --all flag.
+  // If running inside a message type directory, then use that message type
+  // directory.
+  if (resourceDirCtx) {
+    return { type: "messageTypeDir", context: resourceDirCtx };
+  }
+
+  return ux.error("Missing 1 required arg:\nmessageTypeKey");
+};
---
+++ src/lib/marshal/message-type/writer.ts
@@ -0,0 +1,179 @@
+import * as path from "node:path";
+
+import * as fs from "fs-extra";
+import { uniqueId } from "lodash";
+
+import { sandboxDir } from "@/lib/helpers/const";
+import { DirContext } from "@/lib/helpers/fs";
+import { DOUBLE_SPACES } from "@/lib/helpers/json";
+import { WithAnnotation } from "@/lib/marshal/shared/types";
+import { MessageTypeDirContext } from "@/lib/run-context";
+
+import { isMessageTypeDir } from "./helpers";
+import {
+  buildMessageTypeDirBundle,
+  MESSAGE_TYPE_JSON,
+  MessageTypeDirBundle,
+} from "./processor.isomorphic";
+import { readMessageTypeDir } from "./reader";
+import { MessageTypeData } from "./types";
+
+/*
+ * The main write function that takes the fetched message type data from Knock
+ * API (remote message type), and reads the same message type from the local file
+ * system (local message type, if available), then writes the remote message type
+ * into a message type directory with the local message type as a reference.
+ */
+export const writeMessageTypeDirFromData = async (
+  messageTypeDirCtx: MessageTypeDirContext,
+  remoteMessageType: MessageTypeData<WithAnnotation>,
+): Promise<void> => {
+  // If the message type directory exists on the file system (i.e. previously
+  // pulled before), then read the message type file to use as a reference.
+  const [localMessageType] = messageTypeDirCtx.exists
+    ? await readMessageTypeDir(messageTypeDirCtx, { withExtractedFiles: true })
+    : [];
+
+  const bundle = buildMessageTypeDirBundle(remoteMessageType, localMessageType);
+
+  return writeMessageTypeDirFromBundle(messageTypeDirCtx, bundle);
+};
+
+/*
+ * A lower level write function that takes a constructed message type dir bundle
+ * and writes it into a message type directory on a local file system.
+ *
+ * It does not make any assumptions about how the message type directory bundle
+ * was built; for example, it can be from parsing the message type data fetched
+ * from the Knock API, or built manually for scaffolding purposes.
+ */
+const writeMessageTypeDirFromBundle = async (
+  messageTypeDirCtx: MessageTypeDirContext,
+  messageTypeDirBundle: MessageTypeDirBundle,
+): Promise<void> => {
+  const backupDirPath = path.resolve(sandboxDir, uniqueId("backup"));
+
+  try {
+    if (messageTypeDirCtx.exists) {
+      await fs.copy(messageTypeDirCtx.abspath, backupDirPath);
+      await fs.emptyDir(messageTypeDirCtx.abspath);
+    }
+
+    const promises = Object.entries(messageTypeDirBundle).map(
+      ([relpath, fileContent]) => {
+        const filePath = path.resolve(messageTypeDirCtx.abspath, relpath);
+
+        return relpath === MESSAGE_TYPE_JSON
+          ? fs.outputJson(filePath, fileContent, { spaces: DOUBLE_SPACES })
+          : fs.outputFile(filePath, fileContent ?? "");
+      },
+    );
+    await Promise.all(promises);
+  } catch (error) {
+    // In case of any error, wipe the target directory that is likely in a bad
+    // state then restore the backup if one existed before.
+    if (messageTypeDirCtx.exists) {
+      await fs.emptyDir(messageTypeDirCtx.abspath);
+      await fs.copy(backupDirPath, messageTypeDirCtx.abspath);
+    } else {
+      await fs.remove(messageTypeDirCtx.abspath);
+    }
+
+    throw error;
+  } finally {
+    // Always clean up the backup directory in the temp sandbox.
+    await fs.remove(backupDirPath);
+  }
+};
+
+/*
+ * Prunes the index directory by removing any files, or directories that aren't
+ * message type dirs found in fetched message types. We want to preserve any
+ * message type dirs that are going to be updated with remote message types, so
+ * extracted links can be respected.
+ */
+const pruneMessageTypesIndexDir = async (
+  indexDirCtx: DirContext,
+  remoteMessageTypes: MessageTypeData<WithAnnotation>[],
+): Promise<void> => {
+  const messageTypesByKey = Object.fromEntries(
+    remoteMessageTypes.map((w) => [w.key.toLowerCase(), w]),
+  );
+
+  const dirents = await fs.readdir(indexDirCtx.abspath, {
+    withFileTypes: true,
+  });
+
+  const promises = dirents.map(async (dirent) => {
+    const direntName = dirent.name.toLowerCase();
+    const direntPath = path.resolve(indexDirCtx.abspath, direntName);
+
+    if ((await isMessageTypeDir(direntPath)) && messageTypesByKey[direntName]) {
+      return;
+    }
+
+    await fs.remove(direntPath);
+  });
+
+  await Promise.all(promises);
+};
+
+/*
+ * The bulk write function that takes the fetched message types data from Knock
+ * API (remote message types), and writes them into a message types "index"
+ * directory by referencing locally available message types.
+ */
+export const writeMessageTypesIndexDir = async (
+  indexDirCtx: DirContext,
+  remoteMessageTypes: MessageTypeData<WithAnnotation>[],
+): Promise<void> => {
+  const backupDirPath = path.resolve(sandboxDir, uniqueId("backup"));
+
+  try {
+    // If the index directory already exists, back it up in the temp sandbox
+    // before wiping it clean.
+    if (indexDirCtx.exists) {
+      await fs.copy(indexDirCtx.abspath, backupDirPath);
+      await pruneMessageTypesIndexDir(indexDirCtx, remoteMessageTypes);
+    }
+
+    // Write given remote message types into the given message types dir path.
+    const promises = remoteMessageTypes.map(async (messageType) => {
+      const messageTypeDirPath = path.resolve(
+        indexDirCtx.abspath,
+        messageType.key,
+      );
+
+      const messageTypeDirCtx: MessageTypeDirContext = {
+        type: "message_type",
+        key: messageType.key,
+        abspath: messageTypeDirPath,
+        exists: indexDirCtx.exists
+          ? await isMessageTypeDir(messageTypeDirPath)
+          : false,
+      };
+
+      return writeMessageTypeDirFromData(messageTypeDirCtx, messageType);
+    });
+
+    await Promise.all(promises);
+  } catch (error) {
+    console.log(error);
+    // In case of any error, wipe the index directory that is likely in a bad
+    // state then restore the backup if one existed before.
+    if (indexDirCtx.exists) {
+      await fs.emptyDir(indexDirCtx.abspath);
+      await fs.copy(backupDirPath, indexDirCtx.abspath);
+    } else {
+      await fs.remove(indexDirCtx.abspath);
+    }
+
+    throw error;
+  } finally {
+    // Always clean up the backup directory in the temp sandbox.
+    await fs.remove(backupDirPath);
+  }
+};
+
+// // Exported for tests.
+// export { pruneMessageTypesIndexDir };
