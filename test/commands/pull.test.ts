046c69b8c1b113b3890651c6297913271c8f0bfe
Matthew Mikolay <mmikolay@knock.app>
2025-06-02 11:29:39 -0400

    Update UTs

--- test/commands/pull.test.ts
+++ test/commands/pull.test.ts
@@ -1,14 +1,14 @@
 import { expect, test } from "@oclif/test";
 
 describe("commands/pull", () => {
-  // describe("given a valid service token via flag", () => {
-  //   test
-  //     .stdout()
-  //     .command(["pull"])
-  //     .it("runs the command", (ctx) => {
-  //       expect(ctx.stdout).to.contain("TODO");
-  //     });
-  // });
+  describe("given a valid service token via flag", () => {
+    test
+      .stdout()
+      .command(["pull", "--service-token", "valid-token"])
+      .it("runs the command", (ctx) => {
+        expect(ctx.stdout).to.contain("TODO");
+      });
+  });
 
   describe("given a valid service token via env var", () => {
     test
