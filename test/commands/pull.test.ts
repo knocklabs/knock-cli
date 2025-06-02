0a9744ca4c86a80b37f3d060ea724270ed644096
Matthew Mikolay <mmikolay@knock.app>
2025-06-02 11:28:02 -0400

    Add UT

---
+++ test/commands/pull.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from "@oclif/test";
+
+describe("commands/pull", () => {
+  // describe("given a valid service token via flag", () => {
+  //   test
+  //     .stdout()
+  //     .command(["pull"])
+  //     .it("runs the command", (ctx) => {
+  //       expect(ctx.stdout).to.contain("TODO");
+  //     });
+  // });
+
+  describe("given a valid service token via env var", () => {
+    test
+      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+      .stdout()
+      .command(["pull"])
+      .it("runs the command", (ctx) => {
+        expect(ctx.stdout).to.contain("TODO");
+      });
+  });
+
+  describe("given no service token flag", () => {
+    test.command(["pull"]).exit(2).it("exits with status 2");
+  });
+});
