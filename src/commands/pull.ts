47732deb23a3c989bd5e19ba0ed315c635f56d73
Matthew Mikolay <mmikolay@knock.app>
2025-06-02 11:32:11 -0400

    Add environment flag

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -1,8 +1,17 @@
+import { Flags } from "@oclif/core";
+
 import BaseCommand from "@/lib/base-command";
 
 export default class Pull extends BaseCommand<typeof Pull> {
   static summary =
     "Pull all resources from an environment into a local file system.";
+
+  static flags = {
+    environment: Flags.string({
+      default: "development",
+      summary: "The environment to use.",
+    }),
+  };
 
   public async run(): Promise<void> {
     // TODO
