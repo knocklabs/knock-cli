540d93bd48d1b39054219f1056ca89f61aaf33b3
Matthew Mikolay <mmikolay@knock.app>
2025-06-03 17:27:42 -0400

    Update UTs

--- test/commands/pull.test.ts
+++ test/commands/pull.test.ts
@@ -1,24 +1,39 @@
-import { expect, test } from "@oclif/test";
+import { test } from "@oclif/test";
+import * as fs from "fs-extra";
+
+import { sandboxDir } from "@/lib/helpers/const";
+
+const currCwd = process.cwd();
 
 describe("commands/pull", () => {
-  describe("given a valid service token via flag", () => {
-    test
-      .stdout()
-      .command(["pull", "--service-token", "valid-token"])
-      .it("runs the command", (ctx) => {
-        expect(ctx.stdout).to.contain("TODO");
-      });
+  beforeEach(() => {
+    fs.removeSync(sandboxDir);
+    fs.ensureDirSync(sandboxDir);
+    process.chdir(sandboxDir);
+  });
+  afterEach(() => {
+    process.chdir(currCwd);
+    fs.removeSync(sandboxDir);
   });
 
-  describe("given a valid service token via env var", () => {
-    test
-      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
-      .stdout()
-      .command(["pull"])
-      .it("runs the command", (ctx) => {
-        expect(ctx.stdout).to.contain("TODO");
-      });
-  });
+  // describe("given a valid service token via flag", () => {
+  //   test
+  //     .stdout()
+  //     .command(["pull", "--service-token", "valid-token"])
+  //     .it("runs the command", (ctx) => {
+  //       expect(ctx.stdout).to.contain("TODO");
+  //     });
+  // });
+
+  // describe("given a valid service token via env var", () => {
+  //   test
+  //     .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+  //     .stdout()
+  //     .command(["pull"])
+  //     .it("runs the command", (ctx) => {
+  //       expect(ctx.stdout).to.contain("TODO");
+  //     });
+  // });
 
   describe("given no service token flag", () => {
     test.command(["pull"]).exit(2).it("exits with status 2");
