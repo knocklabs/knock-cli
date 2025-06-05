a27b572d0607ab82be70f70ac533c685f4ec3c44
Matthew Mikolay <mmikolay@knock.app>
2025-06-05 13:02:05 -0400

    feat(KNO-8790): add command to pull all resources (#530)

---
+++ src/commands/pull.ts
@@ -0,0 +1,81 @@
+import * as path from "node:path";
+
+import { Flags } from "@oclif/core";
+
+import BaseCommand from "@/lib/base-command";
+import * as CustomFlags from "@/lib/helpers/flag";
+import { promptToConfirm } from "@/lib/helpers/ux";
+
+import EmailLayoutPull from "./layout/pull";
+import PartialPull from "./partial/pull";
+import TranslationPull from "./translation/pull";
+import WorkflowPull from "./workflow/pull";
+
+export default class Pull extends BaseCommand<typeof Pull> {
+  static summary =
+    "Pull all resources from an environment into a local file system.";
+
+  static flags = {
+    environment: Flags.string({
+      default: "development",
+      summary: "The environment to use.",
+    }),
+    "knock-dir": CustomFlags.dirPath({
+      summary: "The target directory path to pull all resources into.",
+      required: true,
+    }),
+    "hide-uncommitted-changes": Flags.boolean({
+      summary: "Hide any uncommitted changes.",
+    }),
+    force: Flags.boolean({
+      summary: "Remove the confirmation prompt.",
+    }),
+  };
+
+  public async run(): Promise<void> {
+    const { flags } = this.props;
+    const targetDirCtx = flags["knock-dir"];
+
+    const prompt = targetDirCtx.exists
+      ? `Pull latest resources into ${targetDirCtx.abspath}?\n  This will overwrite the contents of this directory.`
+      : `Create a new resources directory at ${targetDirCtx.abspath}?`;
+
+    const input = flags.force || (await promptToConfirm(prompt));
+    if (!input) return;
+
+    const args = [
+      "--all",
+      "--environment",
+      flags.environment,
+      ...(flags["hide-uncommitted-changes"]
+        ? ["--hide-uncommitted-changes"]
+        : []),
+      ...(flags["service-token"]
+        ? ["--service-token", flags["service-token"]]
+        : []),
+      // Always use the force flag to skip prompts
+      "--force",
+    ];
+
+    await EmailLayoutPull.run([
+      ...args,
+      "--layouts-dir",
+      path.resolve(targetDirCtx.abspath, "layouts"),
+    ]);
+    await PartialPull.run([
+      ...args,
+      "--partials-dir",
+      path.resolve(targetDirCtx.abspath, "partials"),
+    ]);
+    await TranslationPull.run([
+      ...args,
+      "--translations-dir",
+      path.resolve(targetDirCtx.abspath, "translations"),
+    ]);
+    await WorkflowPull.run([
+      ...args,
+      "--workflows-dir",
+      path.resolve(targetDirCtx.abspath, "workflows"),
+    ]);
+  }
+}
---
+++ test/commands/pull.test.ts
@@ -0,0 +1,336 @@
+import * as path from "node:path";
+
+import { expect, test } from "@oclif/test";
+import enquirer from "enquirer";
+import * as fs from "fs-extra";
+import { isEqual } from "lodash";
+import * as sinon from "sinon";
+
+import { factory } from "@/../test/support";
+import KnockApiV1 from "@/lib/api-v1";
+import { sandboxDir } from "@/lib/helpers/const";
+import { EmailLayoutData } from "@/lib/marshal/email-layout";
+import { PartialData } from "@/lib/marshal/partial";
+import { TranslationData } from "@/lib/marshal/translation";
+import { WorkflowData } from "@/lib/marshal/workflow";
+
+const currCwd = process.cwd();
+
+const setupWithListStubs = (
+  manyLayoutsAttrs: Partial<EmailLayoutData>[],
+  manyPartialsAttrs: Partial<PartialData>[],
+  manyTranslationsAttrs: Partial<TranslationData>[],
+  manyWorkflowAttrs: Partial<WorkflowData>[],
+) =>
+  test
+    .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+    .stub(KnockApiV1.prototype, "listEmailLayouts", (stub) =>
+      stub.resolves(
+        factory.resp({
+          data: {
+            entries: manyLayoutsAttrs.map((attrs) =>
+              factory.emailLayout(attrs),
+            ),
+            page_info: factory.pageInfo(),
+          },
+        }),
+      ),
+    )
+    .stub(KnockApiV1.prototype, "listPartials", (stub) =>
+      stub.resolves(
+        factory.resp({
+          data: {
+            entries: manyPartialsAttrs.map((attrs) => factory.partial(attrs)),
+            page_info: factory.pageInfo(),
+          },
+        }),
+      ),
+    )
+    .stub(KnockApiV1.prototype, "listTranslations", (stub) =>
+      stub.resolves(
+        factory.resp({
+          data: {
+            entries: manyTranslationsAttrs.map((attrs) =>
+              factory.translation(attrs),
+            ),
+            page_info: factory.pageInfo(),
+          },
+        }),
+      ),
+    )
+    .stub(KnockApiV1.prototype, "listWorkflows", (stub) =>
+      stub.resolves(
+        factory.resp({
+          data: {
+            entries: manyWorkflowAttrs.map((attrs) => factory.workflow(attrs)),
+            page_info: factory.pageInfo(),
+          },
+        }),
+      ),
+    )
+    .stub(enquirer.prototype, "prompt", (stub) =>
+      stub.onFirstCall().resolves({ input: "y" }),
+    );
+
+describe("commands/pull", () => {
+  beforeEach(() => {
+    fs.removeSync(sandboxDir);
+    fs.ensureDirSync(sandboxDir);
+    process.chdir(sandboxDir);
+  });
+  afterEach(() => {
+    process.chdir(currCwd);
+    fs.removeSync(sandboxDir);
+  });
+
+  describe("without environment flag", () => {
+    setupWithListStubs(
+      [{ key: "messages" }, { key: "transactional" }],
+      [{ key: "partial-a" }, { key: "partial-b" }],
+      [{ locale_code: "en" }, { locale_code: "es-MX" }],
+      [{ key: "workflow-a" }, { key: "workflow-bar" }],
+    )
+      .stdout()
+      .command(["pull", "--knock-dir", "."])
+      .it(
+        "calls apiV1 to list resources in the development environment",
+        () => {
+          assertApiV1ListFunctionsCalled({ environment: "development" });
+          sinon.assert.calledOnce(enquirer.prototype.prompt as any);
+        },
+      );
+  });
+
+  describe("with environment flag", () => {
+    setupWithListStubs(
+      [{ key: "messages" }, { key: "transactional" }],
+      [{ key: "partial-a" }, { key: "partial-b" }],
+      [{ locale_code: "en" }, { locale_code: "es-MX" }],
+      [{ key: "workflow-a" }, { key: "workflow-bar" }],
+    )
+      .stdout()
+      .command(["pull", "--knock-dir", ".", "--environment", "staging"])
+      .it("calls apiV1 to list resources in the given environment", () => {
+        assertApiV1ListFunctionsCalled({ environment: "staging" });
+        sinon.assert.calledOnce(enquirer.prototype.prompt as any);
+      });
+  });
+
+  describe("with hide-uncommitted-changes flag", () => {
+    setupWithListStubs(
+      [{ key: "messages" }, { key: "transactional" }],
+      [{ key: "partial-a" }, { key: "partial-b" }],
+      [{ locale_code: "en" }, { locale_code: "es-MX" }],
+      [{ key: "workflow-a" }, { key: "workflow-bar" }],
+    )
+      .stdout()
+      .command(["pull", "--knock-dir", ".", "--hide-uncommitted-changes"])
+      .it(
+        "calls apiV1 to list resources with uncommitted changes hidden",
+        () => {
+          assertApiV1ListFunctionsCalled({ "hide-uncommitted-changes": true });
+          sinon.assert.calledOnce(enquirer.prototype.prompt as any);
+        },
+      );
+  });
+
+  describe("with service-token flag", () => {
+    setupWithListStubs(
+      [{ key: "messages" }, { key: "transactional" }],
+      [{ key: "partial-a" }, { key: "partial-b" }],
+      [{ locale_code: "en" }, { locale_code: "es-MX" }],
+      [{ key: "workflow-a" }, { key: "workflow-bar" }],
+    )
+      .env({ KNOCK_SERVICE_TOKEN: null })
+      .stdout()
+      .command(["pull", "--knock-dir", ".", "--service-token=token123"])
+      .it("calls apiV1 to list resources using the given service token", () => {
+        assertApiV1ListFunctionsCalled({ "service-token": "token123" });
+        sinon.assert.calledOnce(enquirer.prototype.prompt as any);
+      });
+  });
+
+  describe("with force flag", () => {
+    setupWithListStubs(
+      [{ key: "messages" }, { key: "transactional" }],
+      [{ key: "partial-a" }, { key: "partial-b" }],
+      [{ locale_code: "en" }, { locale_code: "es-MX" }],
+      [{ key: "workflow-a" }, { key: "workflow-bar" }],
+    )
+      .stdout()
+      .command(["pull", "--knock-dir", ".", "--force"])
+      .it("skips the confirmation prompt", () => {
+        assertApiV1ListFunctionsCalled({ environment: "development" });
+        sinon.assert.notCalled(enquirer.prototype.prompt as any);
+      });
+  });
+
+  setupWithListStubs(
+    [{ key: "layout1" }, { key: "layout2" }],
+    [{ key: "partial1" }, { key: "partial2" }],
+    [{ locale_code: "en" }, { locale_code: "es-MX" }],
+    [{ key: "workflow1" }, { key: "workflow2" }],
+  )
+    .stdout()
+    .command(["pull", "--knock-dir", "resources"])
+    .it(
+      "writes directories to the file system, with individual dirs inside",
+      () => {
+        const path1 = path.resolve(
+          sandboxDir,
+          "resources",
+          "layouts",
+          "layout1",
+        );
+        expect(fs.pathExistsSync(path1)).to.equal(true);
+
+        const path2 = path.resolve(
+          sandboxDir,
+          "resources",
+          "layouts",
+          "layout2",
+        );
+        expect(fs.pathExistsSync(path2)).to.equal(true);
+
+        const path3 = path.resolve(
+          sandboxDir,
+          "resources",
+          "partials",
+          "partial1",
+        );
+        expect(fs.pathExistsSync(path3)).to.equal(true);
+
+        const path4 = path.resolve(
+          sandboxDir,
+          "resources",
+          "partials",
+          "partial2",
+        );
+        expect(fs.pathExistsSync(path4)).to.equal(true);
+
+        const path5 = path.resolve(
+          sandboxDir,
+          "resources",
+          "translations",
+          "en",
+        );
+        expect(fs.pathExistsSync(path5)).to.equal(true);
+
+        const path6 = path.resolve(
+          sandboxDir,
+          "resources",
+          "translations",
+          "es-MX",
+        );
+        expect(fs.pathExistsSync(path6)).to.equal(true);
+
+        const path7 = path.resolve(
+          sandboxDir,
+          "resources",
+          "workflows",
+          "workflow1",
+        );
+        expect(fs.pathExistsSync(path7)).to.equal(true);
+
+        const path8 = path.resolve(
+          sandboxDir,
+          "resources",
+          "workflows",
+          "workflow2",
+        );
+        expect(fs.pathExistsSync(path8)).to.equal(true);
+      },
+    );
+
+  describe("without service token", () => {
+    test.command(["pull"]).exit(2).it("exits with status 2");
+  });
+});
+
+function assertApiV1ListFunctionsCalled(
+  expectedFlags: Record<string, unknown> = {},
+) {
+  sinon.assert.calledWith(
+    KnockApiV1.prototype.listEmailLayouts as any,
+    sinon.match(
+      ({ args, flags }) =>
+        isEqual(args, {}) &&
+        isEqual(flags, {
+          all: true,
+          "layouts-dir": {
+            abspath: path.resolve(sandboxDir, "layouts"),
+            exists: false,
+          },
+          "service-token": "valid-token",
+          environment: "development",
+          force: true,
+          annotate: true,
+          limit: 100,
+          ...expectedFlags,
+        }),
+    ),
+  );
+
+  sinon.assert.calledWith(
+    KnockApiV1.prototype.listPartials as any,
+    sinon.match(
+      ({ args, flags }) =>
+        isEqual(args, {}) &&
+        isEqual(flags, {
+          all: true,
+          "partials-dir": {
+            abspath: path.resolve(sandboxDir, "partials"),
+            exists: false,
+          },
+          "service-token": "valid-token",
+          environment: "development",
+          force: true,
+          annotate: true,
+          limit: 100,
+          ...expectedFlags,
+        }),
+    ),
+  );
+
+  sinon.assert.calledWith(
+    KnockApiV1.prototype.listTranslations as any,
+    sinon.match(
+      ({ args, flags }) =>
+        isEqual(args, {}) &&
+        isEqual(flags, {
+          all: true,
+          "translations-dir": {
+            abspath: path.resolve(sandboxDir, "translations"),
+            exists: false,
+          },
+          "service-token": "valid-token",
+          environment: "development",
+          force: true,
+          limit: 100,
+          format: "json",
+          ...expectedFlags,
+        }),
+    ),
+  );
+
+  sinon.assert.calledWith(
+    KnockApiV1.prototype.listWorkflows as any,
+    sinon.match(
+      ({ args, flags }) =>
+        isEqual(args, {}) &&
+        isEqual(flags, {
+          all: true,
+          "workflows-dir": {
+            abspath: path.resolve(sandboxDir, "workflows"),
+            exists: false,
+          },
+          "service-token": "valid-token",
+          environment: "development",
+          force: true,
+          annotate: true,
+          limit: 100,
+          ...expectedFlags,
+        }),
+    ),
+  );
+}
