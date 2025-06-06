4d2ed710acee5e460995c607da0eb956a7276199
Matthew Mikolay <mmikolay@knock.app>
2025-06-06 15:52:25 -0400

    Push email layouts

--- src/commands/push.ts
+++ src/commands/push.ts
@@ -1,8 +1,13 @@
+import * as path from "node:path";
+
 import { Flags } from "@oclif/core";
+import * as fs from "fs-extra";
 
 import BaseCommand from "@/lib/base-command";
 import { KnockEnv } from "@/lib/helpers/const";
 import * as CustomFlags from "@/lib/helpers/flag";
+
+import EmailLayoutPush from "./layout/push";
 
 export default class Push extends BaseCommand<typeof Push> {
   static summary = "Push all resources from a local file system to Knock.";
@@ -29,6 +34,32 @@ export default class Push extends BaseCommand<typeof Push> {
   };
 
   async run(): Promise<void> {
-    // TODO
+    const { flags } = this.props;
+
+    const targetDirCtx = flags["knock-dir"];
+
+    if (!targetDirCtx.exists) {
+      this.error(`Directory ${targetDirCtx.abspath} does not exist`);
+    }
+
+    const args = [
+      "--all",
+      "--environment",
+      flags.environment,
+      ...(flags["service-token"]
+        ? ["--service-token", flags["service-token"]]
+        : []),
+      ...(flags.commit ? ["--commit"] : []),
+      ...(flags["commit-message"]
+        ? ["--commit-message", flags["commit-message"]]
+        : []),
+    ];
+
+    const layoutsPath = path.resolve(targetDirCtx.abspath, "layouts");
+    const hasLayouts = await fs.pathExists(layoutsPath);
+
+    if (hasLayouts) {
+      await EmailLayoutPush.run([...args, "--layouts-dir", layoutsPath]);
+    }
   }
 }
