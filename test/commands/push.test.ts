7b6854cbe31716314db8822f457fbceafefeb7ba
Matthew Mikolay <mmikolay@knock.app>
2025-06-09 10:25:53 -0400

    Add TODO comments to test file

--- test/commands/push.test.ts
+++ test/commands/push.test.ts
@@ -3,6 +3,10 @@ import { test } from "@oclif/test";
 const KNOCK_SERVICE_TOKEN = "valid-token";
 
 describe("commands/push", () => {
+  // TODO Test pushes layouts, partials, translations, and workflows
+  // TODO Test command only pushes resources when resource-specific subdirectories exist
+  // TODO Test error thrown when resource-specific directories are empty
+
   describe("with environment other than development", () => {
     test
       .env({ KNOCK_SERVICE_TOKEN })
