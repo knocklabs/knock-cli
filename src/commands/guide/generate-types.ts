05641192180e254dde26586ceb5257c03c598c50
Thomas <thomas.whyyou@gmail.com>
2025-09-03 13:49:57 -0400

    feat: add guide generate-types command (#575)

---
+++ src/commands/guide/generate-types.ts
@@ -0,0 +1,120 @@
+import path from "node:path";
+
+import { Flags } from "@oclif/core";
+import * as fs from "fs-extra";
+
+import BaseCommand from "@/lib/base-command";
+import { KnockEnv } from "@/lib/helpers/const";
+import { ApiError } from "@/lib/helpers/error";
+import * as CustomFlags from "@/lib/helpers/flag";
+import { merge } from "@/lib/helpers/object.isomorphic";
+import { MAX_PAGINATION_LIMIT, PageInfo } from "@/lib/helpers/page";
+import { formatErrorRespMessage, isSuccessResp } from "@/lib/helpers/request";
+import {
+  getLanguageFromExtension,
+  supportedExtensions,
+} from "@/lib/helpers/typegen";
+import { spinner } from "@/lib/helpers/ux";
+import * as Guide from "@/lib/marshal/guide";
+import { WithAnnotation } from "@/lib/marshal/shared/types";
+
+const supportedExts = supportedExtensions.join(", ");
+
+export default class GuideGenerateTypes extends BaseCommand<
+  typeof GuideGenerateTypes
+> {
+  static description =
+    "Generate types for all guides in an environment and write them to a file.";
+
+  static flags = {
+    environment: Flags.string({
+      summary: "Select the environment to generate types for.",
+      default: KnockEnv.Development,
+    }),
+    "output-file": CustomFlags.filePath({
+      summary: `The output file to write the generated types to. We currently support ${supportedExts} files only. Your file extension will determine the target language for the generated types.`,
+      required: true,
+    }),
+  };
+
+  async run(): Promise<void> {
+    const { flags } = this.props;
+
+    const outputFilePath = flags["output-file"].abspath;
+    const extension = path.extname(outputFilePath);
+    const targetLang = getLanguageFromExtension(extension);
+
+    if (!targetLang) {
+      this.error(
+        new ApiError(
+          `Unsupported file extension: ${extension}. We currently support ${supportedExts} files only.`,
+        ),
+      );
+    }
+
+    spinner.start(`‣ Loading guides`);
+
+    // 1. List all guides in the target environment.
+    const guides = await this.listAllGuides();
+
+    spinner.stop();
+
+    // 2. Generate types for all guides and its step contents.
+    spinner.start(`‣ Generating types`);
+
+    const { result, count, mapping } = await Guide.generateTypes(
+      guides,
+      targetLang,
+    );
+
+    spinner.stop();
+
+    if (!result) {
+      this.log(
+        `‣ No guides with content JSON schema found, skipping type generation`,
+      );
+      return;
+    }
+
+    // 3. Write the generated types to the output file.
+    await fs.outputFile(outputFilePath, result.lines.join("\n"));
+
+    // 4. If for typescript, render and append the types index at the end.
+    if (targetLang === "typescript") {
+      const lines = Guide.generateIndexTypeTS(mapping);
+      await fs.appendFile(outputFilePath, lines.join("\n"));
+    }
+
+    this.log(
+      `‣ Successfully generated types for ${count} message type(s) and wrote them to ${outputFilePath}`,
+    );
+  }
+
+  async listAllGuides(
+    pageParams: Partial<PageInfo> = {},
+    guidesFetchedSoFar: Guide.GuideData<WithAnnotation>[] = [],
+  ): Promise<Guide.GuideData<WithAnnotation>[]> {
+    const props = merge(this.props, {
+      flags: {
+        ...pageParams,
+        limit: MAX_PAGINATION_LIMIT,
+        // Generate json schemas for guide contents so we can generate types.
+        "include-json-schema": true,
+      },
+    });
+
+    const resp = await this.apiV1.listGuides<WithAnnotation>(props);
+
+    if (!isSuccessResp(resp)) {
+      const message = formatErrorRespMessage(resp);
+      this.error(new ApiError(message));
+    }
+
+    const { entries, page_info: pageInfo } = resp.data;
+    const guides = [...guidesFetchedSoFar, ...entries];
+
+    return pageInfo.after
+      ? this.listAllGuides({ after: pageInfo.after }, guides)
+      : guides;
+  }
+}
--- src/lib/marshal/guide/helpers.ts
+++ src/lib/marshal/guide/helpers.ts
@@ -2,8 +2,17 @@ import * as path from "node:path";
 
 import { ux } from "@oclif/core";
 import * as fs from "fs-extra";
+import { startCase } from "lodash";
+import {
+  FetchingJSONSchemaStore,
+  InputData,
+  JSONSchemaInput,
+  quicktype,
+  SerializedRenderResult,
+} from "quicktype-core";
 
 import { DirContext } from "@/lib/helpers/fs";
+import { SupportedTypeLanguage } from "@/lib/helpers/typegen";
 import { GuideDirContext, RunContext } from "@/lib/run-context";
 
 import { GUIDE_JSON } from "./processor.isomorphic";
@@ -154,4 +163,151 @@ export const ensureValidCommandTarget = async (
   }
 
   return ux.error("Missing 1 required arg:\nguideKey");
+};
+
+/*
+ * Takes an array of guides and generate types from its content json schemas.
+ */
+const SCHEMA_TITLE_PREFIX = "Guide";
+
+// Content types organized by guide key and type.
+type ContentTypesMapping = {
+  key: Record<string, string>;
+  type: Record<string, Array<string>>;
+};
+
+export async function generateTypes(
+  guides: GuideData[],
+  targetLanguage: SupportedTypeLanguage,
+): Promise<{
+  result: SerializedRenderResult | undefined;
+  count: number;
+  mapping: ContentTypesMapping;
+}> {
+  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
+
+  let processedCount = 0;
+  const mapping: ContentTypesMapping = { key: {}, type: {} };
+
+  for (const guide of guides) {
+    if (!guide.type) {
+      continue;
+    }
+
+    // We only support single step guides at the moment.
+    const step = (guide.steps || []).find((s) => s.json_schema);
+
+    if (!step) {
+      continue;
+    }
+
+    // Format the type name that quicktype can output exactly. It's important
+    // that the names we format are used/preserved by quicktype as we create the
+    // final mapping referencing these type names.
+    // Example: `GuideBannerFourStep1Banner001Default`
+    const typeName = [
+      SCHEMA_TITLE_PREFIX,
+      startCaseNoSpace(guide.key),
+      startCaseNoSpace(step.ref),
+      startCaseNoSpace(step.schema_key),
+      startCaseNoSpace(step.schema_semver),
+      startCaseNoSpace(step.schema_variant_key),
+    ].join("");
+
+    schemaInput.addSource({
+      name: typeName,
+      schema: JSON.stringify({ ...step.json_schema, title: typeName }),
+    });
+
+    mapping.key[guide.key] = typeName;
+
+    mapping.type[guide.type] = [...(mapping.type[guide.type] || []), typeName];
+
+    processedCount++;
+  }
+
+  if (processedCount === 0) {
+    return { result: undefined, count: 0, mapping };
+  }
+
+  const inputData = new InputData();
+  inputData.addInput(schemaInput);
+
+  const result = await quicktype({
+    inputData,
+    lang: targetLanguage,
+    allPropertiesOptional: false,
+    alphabetizeProperties: true,
+    rendererOptions: {
+      "just-types": true,
+      "no-extra-properties": true,
+      "no-optional-null": true,
+    },
+  });
+
+  return { result, count: processedCount, mapping };
+}
+
+const startCaseNoSpace = (key: string) => startCase(key).replace(/\s/g, "");
+
+/*
+ * For typescript, this writes a root type that maps out all guide types by
+ * guide type and key.
+ *
+ * For example:
+ *   type GuideContentByType = {
+ *     "banner-one": GuideBannerOneStep1Banner001Default;
+ *     "banner-two": GuideBannerTwoStep1Banner001Default;
+ *     "card-one": GuideCardOneStep1Card001Default;
+ *     "card-two": GuideCardTwoStep1Card001SingleAction;
+ *     "changelog-card": GuideChangelogCardStep1ChangelogCard001SingleAction;
+ *     "modal-one": GuideModalOneStep1Modal001MultiAction;
+ *   };
+ *
+ *   type GuideContentByKey = {
+ *     "banner": GuideBannerOneStep1Banner001Default | GuideBannerTwoStep1Banner001Default;
+ *     "card": GuideCardOneStep1Card001Default | GuideCardTwoStep1Card001SingleAction;
+ *     "changelog-card": GuideChangelogCardStep1ChangelogCard001SingleAction;
+ *     "modal": GuideModalOneStep1Modal001MultiAction;
+ *   };
+ *
+ *   export type GuideContentIndex = {
+ *     key: GuideContentByType;
+ *     type: GuideContentByKey;
+ *   };
+ */
+const TS_INDEX_TYPE_PREFIX = "GuideContent";
+
+export const generateIndexTypeTS = (
+  mapping: ContentTypesMapping,
+): Array<string> => {
+  const lines: Array<string> = [];
+
+  // Define the type for sub index by guide key.
+  const byKey = `${TS_INDEX_TYPE_PREFIX}ByKey`;
+  lines.push(`\ntype ${byKey} = {`);
+
+  for (const [key, val] of Object.entries(mapping.key)) {
+    lines.push(`  "${key}": ${val};`);
+  }
+
+  lines.push("};");
+
+  // Define the type for sub index by guide type.
+  const byType = `${TS_INDEX_TYPE_PREFIX}ByType`;
+  lines.push(`\ntype ${byType} = {`);
+
+  for (const [key, val] of Object.entries(mapping.type)) {
+    lines.push(`  "${key}": ${(val || []).join(" | ")};`);
+  }
+
+  lines.push(
+    "};",
+    `\nexport type ${TS_INDEX_TYPE_PREFIX}Index = {`,
+    `  key: ${byKey};`,
+    `  type: ${byType};`,
+    "};",
+  );
+
+  return lines;
 };
