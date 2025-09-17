952d4811e0e0c0e89bc08ef98510d91f41854bd3
Matthew Mikolay <mmikolay@knock.app>
2025-09-17 10:58:51 -0400

    Get pagination working

--- src/commands/branch/list.ts
+++ src/commands/branch/list.ts
@@ -3,6 +3,13 @@ import { ux } from "@oclif/core";
 import * as ApiV1 from "@/lib/api-v1";
 import BaseCommand from "@/lib/base-command";
 import { formatDate } from "@/lib/helpers/date";
+import {
+  maybePromptPageAction,
+  pageFlags,
+  PageParams,
+  paramsForPageAction,
+  toPageParams,
+} from "@/lib/helpers/page";
 import { withSpinnerV2 } from "@/lib/helpers/request";
 
 export default class BranchList extends BaseCommand<typeof BranchList> {
@@ -11,17 +18,30 @@ export default class BranchList extends BaseCommand<typeof BranchList> {
   static summary =
     "Display all existing branches off of the development environment.";
 
+  static flags = {
+    ...pageFlags,
+  };
+
   static enableJsonFlag = true;
 
-  public async run(): Promise<ApiV1.ListBranchResp | void> {
-    const resp = await withSpinnerV2<ApiV1.ListBranchResp>(() =>
-      this.apiV1.knockMgmt.get("/v1/branches"),
-    );
+  async run(): Promise<ApiV1.ListBranchResp | void> {
+    const resp = await this.request();
 
     const { flags } = this.props;
     if (flags.json) return resp;
 
     this.render(resp);
+  }
+
+  async request(pageParams: PageParams = {}): Promise<ApiV1.ListBranchResp> {
+    const queryParams = toPageParams({
+      ...this.props.flags,
+      ...pageParams,
+    });
+
+    return withSpinnerV2<ApiV1.ListBranchResp>(() =>
+      this.apiV1.knockMgmt.get("/v1/branches", { query: queryParams }),
+    );
   }
 
   async render(data: ApiV1.ListBranchResp): Promise<void> {
@@ -49,5 +69,21 @@ export default class BranchList extends BaseCommand<typeof BranchList> {
           entry.last_commit_at ? formatDate(entry.last_commit_at) : "Never",
       },
     });
+
+    return this.prompt(data);
+  }
+
+  async prompt(data: ApiV1.ListBranchResp): Promise<void> {
+    const { page_info } = data;
+
+    const pageAction = await maybePromptPageAction(page_info);
+    const pageParams = pageAction && paramsForPageAction(pageAction, page_info);
+
+    if (pageParams) {
+      this.log("\n");
+
+      const resp = await this.request(pageParams);
+      return this.render(resp);
+    }
   }
 }
