fc425a8af0f0ef028443f70a2730f0023b7b2d90
Matthew Mikolay <mmikolay@knock.app>
2025-09-19 16:16:58 -0400

    feat(KNO-9790): add branch delete command (#597)

---
+++ src/commands/branch/delete.ts
@@ -0,0 +1,42 @@
+import { Flags } from "@oclif/core";
+
+import BaseCommand from "@/lib/base-command";
+import { CustomArgs } from "@/lib/helpers/arg";
+import { withSpinnerV2 } from "@/lib/helpers/request";
+import { promptToConfirm } from "@/lib/helpers/ux";
+
+export default class BranchDelete extends BaseCommand<typeof BranchDelete> {
+  // Hide until branches are released in GA
+  static hidden = true;
+
+  static summary = "Deletes an existing branch with the given slug.";
+
+  static args = {
+    slug: CustomArgs.slugArg({
+      required: true,
+      description: "The slug of the branch to delete",
+    }),
+  };
+
+  static flags = {
+    force: Flags.boolean({
+      summary: "Remove the confirmation prompt.",
+    }),
+  };
+
+  async run(): Promise<void> {
+    const { args, flags } = this.props;
+
+    // Confirm before deleting the branch, unless forced
+    const prompt = `Delete branch \`${args.slug}\`?`;
+    const input = flags.force || (await promptToConfirm(prompt));
+    if (!input) return;
+
+    await withSpinnerV2(
+      () => this.apiV1.mgmtClient.delete(`/v1/branches/${args.slug}`),
+      { action: "‣ Deleting branch" },
+    );
+
+    this.log(`‣ Successfully deleted branch \`${args.slug}\``);
+  }
+}
---
+++ test/commands/branch/delete.test.ts
@@ -0,0 +1,129 @@
+// Don't ask me why, but importing this is necessary to stub KnockMgmt.prototype.delete
+import "@/../test/support";
+
+import KnockMgmt from "@knocklabs/mgmt";
+import { expect, test } from "@oclif/test";
+import enquirer from "enquirer";
+import * as sinon from "sinon";
+
+describe("commands/branch/delete", () => {
+  describe("given confirmation accepted", () => {
+    test
+      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+      .stub(KnockMgmt.prototype, "delete", (stub) => stub.resolves())
+      .stub(enquirer.prototype, "prompt", (stub) =>
+        stub.resolves({ input: "y" }),
+      )
+      .stdout()
+      .command(["branch delete", "test-branch"])
+      .it(
+        "calls knockMgmt.delete with correct parameters and shows success message",
+        (ctx) => {
+          sinon.assert.calledWith(
+            KnockMgmt.prototype.delete as any,
+            "/v1/branches/test-branch",
+          );
+          expect(ctx.stdout).to.contain(
+            "Successfully deleted branch `test-branch`",
+          );
+        },
+      );
+  });
+
+  describe("given --force flag", () => {
+    test
+      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+      .stub(KnockMgmt.prototype, "delete", (stub) => stub.resolves())
+      .stdout()
+      .command(["branch delete", "test-branch", "--force"])
+      .it(
+        "calls knockMgmt.delete without prompting for confirmation",
+        (ctx) => {
+          sinon.assert.calledWith(
+            KnockMgmt.prototype.delete as any,
+            "/v1/branches/test-branch",
+          );
+          expect(ctx.stdout).to.contain(
+            "Successfully deleted branch `test-branch`",
+          );
+        },
+      );
+  });
+
+  describe("given confirmation declined", () => {
+    test
+      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+      .stub(KnockMgmt.prototype, "delete", (stub) => stub.resolves())
+      .stub(enquirer.prototype, "prompt", (stub) =>
+        stub.resolves({ input: false }),
+      )
+      .stdout()
+      .command(["branch delete", "test-branch"])
+      .it(
+        "does not call knockMgmt.delete and shows no success message",
+        (ctx) => {
+          sinon.assert.notCalled(KnockMgmt.prototype.delete as any);
+          expect(ctx.stdout).to.not.contain("Successfully deleted branch");
+        },
+      );
+  });
+
+  describe("given an argument containing mixed casing and whitespace", () => {
+    test
+      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+      .stub(KnockMgmt.prototype, "delete", (stub) => stub.resolves())
+      .command(["branch delete", " Mixed Case   With Whitespace ", "--force"])
+      .it("slugifies input before calling knockMgmt.delete", () => {
+        sinon.assert.calledWith(
+          KnockMgmt.prototype.delete as any,
+          "/v1/branches/mixed-case-with-whitespace",
+        );
+      });
+  });
+
+  describe("given an invalid branch slug", () => {
+    test
+      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+      .command(
+        // Attempts to pass in whitespace as the slug
+        ["branch delete", " "],
+      )
+      .catch(/Invalid slug provided/)
+      .it("throws an error");
+  });
+
+  describe("given no service token", () => {
+    test
+      .command(["branch delete", "test-branch"])
+      .exit(2)
+      .it("exits with status 2");
+  });
+
+  describe("given API error", () => {
+    test
+      .env({ KNOCK_SERVICE_TOKEN: "valid-token" })
+      .stub(KnockMgmt.prototype, "delete", (stub) =>
+        stub.rejects(
+          new KnockMgmt.APIError(
+            404,
+            {
+              code: "branch_not_found",
+              message: "The branch you specified was not found in this project",
+              status: 404,
+              type: "invalid_request_error",
+            },
+            undefined,
+            new Headers(),
+          ),
+        ),
+      )
+      .stub(enquirer.prototype, "prompt", (stub) =>
+        stub.resolves({ input: "y" }),
+      )
+      .command(["branch delete", "nonexistent-branch"])
+      .catch(
+        /The branch you specified was not found in this project \(status: 404\)/,
+      )
+      .it("throws error when API returns error");
+  });
+});
