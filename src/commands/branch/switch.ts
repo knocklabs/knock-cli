18c66a2d2b65778905ba24a7cb00b102afc091e4
Matthew Mikolay <mmikolay@knock.app>
2025-10-08 15:58:55 -0400

    Remove .gitignore logic for now

--- src/commands/branch/switch.ts
+++ src/commands/branch/switch.ts
@@ -1,8 +1,3 @@
-import * as path from "node:path";
-
-import { Flags } from "@oclif/core";
-import * as fs from "fs-extra";
-
 import * as ApiV1 from "@/lib/api-v1";
 import BaseCommand from "@/lib/base-command";
 import { CustomArgs } from "@/lib/helpers/arg";
@@ -12,19 +7,12 @@ import {
 } from "@/lib/helpers/branch";
 import { findFile } from "@/lib/helpers/fs";
 import { withSpinnerV2 } from "@/lib/helpers/request";
-import { promptToConfirm } from "@/lib/helpers/ux";
 
 export default class BranchSwitch extends BaseCommand<typeof BranchSwitch> {
   // Hide until branches are released in GA
   static hidden = true;
 
   static summary = "Switches to an existing branch with the given slug.";
-
-  static flags = {
-    force: Flags.boolean({
-      summary: "Remove the confirmation prompt.",
-    }),
-  };
 
   static args = {
     slug: CustomArgs.slug({
@@ -34,25 +22,14 @@ export default class BranchSwitch extends BaseCommand<typeof BranchSwitch> {
   };
 
   async run(): Promise<void> {
-    const { args, flags } = this.props;
+    const { args } = this.props;
 
     const currDir = process.cwd();
-    let branchFilePath = await findFile(currDir, BRANCH_FILE_NAME);
+    const branchFilePath = await findFile(currDir, BRANCH_FILE_NAME);
 
     if (!branchFilePath) {
-      const gitIgnoreFilePath = await findFile(currDir, ".gitignore");
-      if (!gitIgnoreFilePath) {
-        this.log(`‣ Cannot locate ${BRANCH_FILE_NAME} file, skipping switch`);
-        return;
-      }
-
-      const dirPath = gitIgnoreFilePath.replace("/.gitignore", "");
-      const prompt = `Create a new ${BRANCH_FILE_NAME} file at ${dirPath}?`;
-      const input = flags.force || (await promptToConfirm(prompt));
-      if (!input) return;
-
-      branchFilePath = path.resolve(dirPath, BRANCH_FILE_NAME);
-      await fs.ensureFile(branchFilePath);
+      this.log(`‣ Cannot locate ${BRANCH_FILE_NAME} file, skipping switch`);
+      return;
     }
 
     this.log(`‣ Switching to branch \`${args.slug}\``);
