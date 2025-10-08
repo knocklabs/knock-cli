feca0cced27a286bebcd971226e54a2c20aadbd3
Matthew Mikolay <mmikolay@knock.app>
2025-10-08 14:47:00 -0400

    Create .knock_branch sibling to .gitignore

--- src/commands/branch/switch.ts
+++ src/commands/branch/switch.ts
@@ -1,3 +1,8 @@
+import * as path from "node:path";
+
+import { Flags } from "@oclif/core";
+import * as fs from "fs-extra";
+
 import * as ApiV1 from "@/lib/api-v1";
 import BaseCommand from "@/lib/base-command";
 import { CustomArgs } from "@/lib/helpers/arg";
@@ -7,12 +12,19 @@ import {
   updateCurrentBranchFile,
 } from "@/lib/helpers/branch";
 import { withSpinnerV2 } from "@/lib/helpers/request";
+import { promptToConfirm } from "@/lib/helpers/ux";
 
 export default class BranchSwitch extends BaseCommand<typeof BranchSwitch> {
   // Hide until branches are released in GA
   static hidden = true;
 
   static summary = "Switches to an existing branch with the given slug.";
+
+  static flags = {
+    force: Flags.boolean({
+      summary: "Remove the confirmation prompt.",
+    }),
+  };
 
   static args = {
     slug: CustomArgs.slug({
@@ -22,15 +34,25 @@ export default class BranchSwitch extends BaseCommand<typeof BranchSwitch> {
   };
 
   async run(): Promise<void> {
-    const { args } = this.props;
+    const { args, flags } = this.props;
 
     const currDir = process.cwd();
-    const branchFilePath = await findFile(currDir, BRANCH_FILE_NAME);
+    let branchFilePath = await findFile(currDir, BRANCH_FILE_NAME);
 
     if (!branchFilePath) {
-      // TODO Automatically create .knock_current_branch file
-      this.log(`‣ Cannot locate .knock_current_branch file, skipping switch`);
-      return;
+      const gitIgnoreFilePath = await findFile(currDir, ".gitignore");
+      if (!gitIgnoreFilePath) {
+        this.log(`‣ Cannot locate ${BRANCH_FILE_NAME} file, skipping switch`);
+        return;
+      }
+
+      const dirPath = gitIgnoreFilePath.replace("/.gitignore", "");
+      const prompt = `Create a new ${BRANCH_FILE_NAME} file at ${dirPath}?`;
+      const input = flags.force || (await promptToConfirm(prompt));
+      if (!input) return;
+
+      branchFilePath = path.resolve(dirPath, BRANCH_FILE_NAME);
+      await fs.ensureFile(branchFilePath);
     }
 
     this.log(`‣ Switching to branch \`${args.slug}\``);
