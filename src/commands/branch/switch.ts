60b5ce88111915c5ba00a02293523cbf262877ed
Matthew Mikolay <mmikolay@knock.app>
2025-10-07 21:06:43 -0400

    Fetch the given branch

--- src/commands/branch/switch.ts
+++ src/commands/branch/switch.ts
@@ -1,5 +1,7 @@
+import * as ApiV1 from "@/lib/api-v1";
 import BaseCommand from "@/lib/base-command";
 import { CustomArgs } from "@/lib/helpers/arg";
+import { withSpinnerV2 } from "@/lib/helpers/request";
 
 export default class BranchSwitch extends BaseCommand<typeof BranchSwitch> {
   // Hide until branches are released in GA
@@ -15,6 +17,13 @@ export default class BranchSwitch extends BaseCommand<typeof BranchSwitch> {
   };
 
   async run(): Promise<void> {
-    // TODO
+    const { args } = this.props;
+
+    this.log(`‣ Switching to branch \`${args.slug}\``);
+
+    // Fetch the branch to make sure it exists
+    await withSpinnerV2<ApiV1.BranchData>(() =>
+      this.apiV1.mgmtClient.get(`/v1/branches/${args.slug}`),
+    );
   }
 }
