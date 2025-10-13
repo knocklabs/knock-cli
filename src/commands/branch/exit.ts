b349bb1470408e05a6ce6656cf9dccf112630a90
Matthew Mikolay <mmikolay@knock.app>
2025-10-13 09:44:13 -0400

    Update exit command to use find-up

--- src/commands/branch/exit.ts
+++ src/commands/branch/exit.ts
@@ -1,8 +1,8 @@
+import findUp from "find-up";
 import * as fs from "fs-extra";
 
 import BaseCommand from "@/lib/base-command";
 import { BRANCH_FILE_NAME } from "@/lib/helpers/branch";
-import { findFile } from "@/lib/helpers/fs";
 
 export default class BranchExit extends BaseCommand<typeof BranchExit> {
   // Hide until branches are released in GA
@@ -11,8 +11,7 @@ export default class BranchExit extends BaseCommand<typeof BranchExit> {
   static summary = "Exits the current branch.";
 
   async run(): Promise<void> {
-    const currDir = process.cwd();
-    const branchFilePath = await findFile(currDir, BRANCH_FILE_NAME);
+    const branchFilePath = await findUp(BRANCH_FILE_NAME);
 
     if (!branchFilePath) {
       this.log("‣ No branch is currently active, skipping exit");
