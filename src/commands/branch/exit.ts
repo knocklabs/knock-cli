802bd7506fc829078b3ef1a1af1d9dd2b6c1af86
Matthew Mikolay <mmikolay@knock.app>
2025-10-13 10:03:37 -0400

    feat(KNO-10048): add branch exit command (#623)
    ### Description
    
    This PR adds a new command `knock branch exit`. When the `.knockbranch` file exists, this command will delete it.
    
    ### Tasks
    
    [KNO-10048](https://linear.app/knock/issue/KNO-10048/cli-add-branch-exit-command)

---
+++ src/commands/branch/exit.ts
@@ -0,0 +1,25 @@
+import findUp from "find-up";
+import * as fs from "fs-extra";
+
+import BaseCommand from "@/lib/base-command";
+import { BRANCH_FILE_NAME } from "@/lib/helpers/branch";
+
+export default class BranchExit extends BaseCommand<typeof BranchExit> {
+  // Hide until branches are released in GA
+  static hidden = true;
+
+  static summary = "Exits the current branch.";
+
+  async run(): Promise<void> {
+    const branchFilePath = await findUp(BRANCH_FILE_NAME);
+
+    if (!branchFilePath) {
+      this.log("‣ No branch is currently active, skipping exit");
+      return;
+    }
+
+    await fs.remove(branchFilePath);
+
+    this.log("‣ Successfully exited the branch");
+  }
+}
