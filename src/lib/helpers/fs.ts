403c8a2d739193d790e6bfd165a3b194161acfe2
Matthew Mikolay <mmikolay@knock.app>
2025-10-13 09:34:35 -0400

    Remove findFile altogether

--- src/lib/helpers/fs.ts
+++ src/lib/helpers/fs.ts
@@ -1,5 +1,3 @@
-import * as path from "node:path";
-
 import * as fs from "fs-extra";
 
 export type DirContext = {
@@ -14,32 +12,4 @@ export const isDirectory = async (abspath: string): Promise<boolean> => {
   const exists = await fs.pathExists(abspath);
 
   return exists && (await fs.lstat(abspath)).isDirectory();
-};
-
-/**
- * Finds a file by recursively walking up the directory tree starting from the given directory.
- * @param currDir the directory to start searching from
- * @param fileName the name of the file to find
- * @returns the path to the file, or `undefined` if no file is found
- */
-export const findFile = async (
-  currDir: string,
-  fileName: string,
-): Promise<string | undefined> => {
-  const filePath = path.resolve(currDir, fileName);
-  const fileExists = await fs.pathExists(filePath);
-
-  if (fileExists) {
-    return filePath;
-  }
-
-  // If we reached the root of the filesystem, nothing more to do
-  const { root } = path.parse(currDir);
-  if (currDir === root) {
-    return undefined;
-  }
-
-  // Keep walking up the directory tree
-  const parentDir = path.resolve(currDir, "..");
-  return findFile(parentDir, fileName);
 };
