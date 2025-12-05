26e3787abfc0790eaca2526e7f042d0c80cd4cd6
Matthew Mikolay <mmikolay@knock.app>
2025-12-05 10:11:40 -0500

    Add prompt

--- src/commands/branch/merge.ts
+++ src/commands/branch/merge.ts
@@ -1,5 +1,8 @@
+import { Flags } from "@oclif/core";
+
 import BaseCommand from "@/lib/base-command";
 import { CustomArgs } from "@/lib/helpers/arg";
+import { promptToConfirm } from "@/lib/helpers/ux";
 
 export default class BranchMerge extends BaseCommand<typeof BranchMerge> {
   static summary = "Merges a branch into the development environment.";
@@ -11,7 +14,18 @@ export default class BranchMerge extends BaseCommand<typeof BranchMerge> {
     }),
   };
 
+  static flags = {
+    force: Flags.boolean({
+      summary: "Remove the confirmation prompt.",
+    }),
+  };
+
   async run(): Promise<void> {
+    const { args, flags } = this.props;
+    const prompt = `Merge all changes from branch \`${args.slug}\` into the development environment?`;
+    const input = flags.force || (await promptToConfirm(prompt));
+    if (!input) return;
+
     // TODO
   }
 }
